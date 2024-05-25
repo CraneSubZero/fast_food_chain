47f4996dd09b9f2185713b956f2e2941a84d237c
Mae Ann Pacquiao <maepacquiao0405@gmail.com>
2024-05-25 21:47:38 +0800

    modified

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
                             class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                                class="fa fa-laptop me-2"></i>Elements</a>
+                                class="fa fa-laptop me-2"></i>Fast Food Chains</a>
                         <div class="dropdown-menu bg-transparent border-0">
                            <a href="chains.php" class="dropdown-item">Chains</a>
                             <a href="products.php" class="dropdown-item">Product</a>
